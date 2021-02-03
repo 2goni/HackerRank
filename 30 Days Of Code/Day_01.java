c0baeb4e0214c2a59394ccfdba49513fd0a5f418
2goni <gh2684@naver.com>
2021-02-03 10:46:52 +0900

    Answer

---
+++ 30 Days Of Code/Day_01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Day_01 {
+
+	public static void main(String[] args) {
+		
+        /* Declare second integer, double, and String variables. */
+        int num = 0;
+        double dnum = 0;
+        String str = "";
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        dnum = sc.nextDouble();
+        sc.nextLine();
+        str = sc.nextLine();
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(num + i);
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(dnum + d);		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s.concat(str));
+        scan.close();
+	}
+
+}
