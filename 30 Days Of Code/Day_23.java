794b8004fd23024faae4024e7d4d0808635888de
2goni <gh2684@naver.com>
2021-02-26 12:02:50 +0900

    Answer

---
+++ 30 Days Of Code/Day_23.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Node {
+	Node left, right;
+	int data;
+
+	Node(int data) {
+		this.data = data;
+		left = right = null;
+	}
+}
+
+class Day_23 {
+
+	static void levelOrder(Node root) {
+		// Write your code here
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+			Node print = queue.remove();
+			System.out.print(print.data + " ");
+			if (print.left != null) {
+				queue.add(print.left);
+			}
+			if (print.right != null) {
+				queue.add(print.right);
+			}
+		}
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		Node root = null;
+		while (T-- > 0) {
+			int data = sc.nextInt();
+			root = insert(root, data);
+		}
+		levelOrder(root);
+	}
+}
